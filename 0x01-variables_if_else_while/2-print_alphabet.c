93ad7d908daa455997951a6fa57d96bcddff340c
mr-mensah <edmundmensah422@gmail.com>
2023-06-16 11:14:02 -0700

    lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,9 +11,9 @@ int main(void)
 	char a[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 	for (i = 0; i < 26; i++)
-	{
-		putchar(a[i]);
-	}
-	putchar('\n');
-	return (0);
+{
+putchar(a[i]);
+}
+putchar('\n');
+return (0);
 }
