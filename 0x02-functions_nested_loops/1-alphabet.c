689d18f2cb6fbc18c4e5187f176946e4977a0837
mr-mensah <edmundmensah422@gmail.com>
2023-06-20 13:13:47 -0700

    insomia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * alphabet - first entry point
+ * print_alphabet - first entry point
  * Return: Alays 0(Success)
  */
-void alphabet(void)
+void print_alphabet(void)
 {
 char alpha;
 for (alpha = 'a'; alpha <= 'z'; alpha++)
