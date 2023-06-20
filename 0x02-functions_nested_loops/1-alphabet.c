051b2b017188d35b380ca1e194c30647d734d93e
mr-mensah <edmundmensah422@gmail.com>
2023-06-20 13:06:11 -0700

    insomia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - first entry point 
+ *
+ * Return: Alays 0(Success)
+ */
+void print_alphabet(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+_putchar(alpha);
+_putchar('\n');
+}
