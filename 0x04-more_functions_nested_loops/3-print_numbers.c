cdcfd0c6dfd7d1ea9aac60a0fe0999640203cec2
mr-mensah <edmundmensah422@gmail.com>
2023-06-23 05:52:25 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - print 0 to 9
+ * return: void
+ */
+void print_numbers(void)
+{
+char a;
+for (a = '0'; a <= '9'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
