5c0dddf53d5f469168b0b88ae4eb6a0d84bcae23
mr-mensah <edmundmensah422@gmail.com>
2023-07-07 17:14:52 -0700

    stress

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,41 +4,40 @@
 /**
  * _atoi - program that multiplies two numbers
  * @s: string
- * Return: j
+ * Return: 1
  */
 int _atoi(char *s)
 {
-int i, j, k, l, m, n;
+int i, d, n, len, f, digit;
 i = 0;
-j = 0;
-k = 0;
-l = 0;
-m = 0;
+d = 0;
 n = 0;
-while (s[l] != '\0')
-l++;
-while (i < l && m == 0)
+len = 0;
+f = 0;
+digit = 0;
+while (s[len] != '\0')
+len++;
+while (i < len && f == 0)
 {
 if (s[i] == '-')
-++j;
+++d;
 if (s[i] >= '0' && s[i] <= '9')
 {
-n = s[i] - '0';
-if (j % 2)
-n = -n;
-k = k * 10 + n;
-m = 1;
+digit = s[i] - '0';
+if (d % 2)
+digit = -digit;
+n = n * 10 + digit;
+f = 1;
 if (s[i + 1] < '0' || s[i + 1] > '9')
 break;
-m = 0;
+f = 0;
 }
 i++;
 }
-if (m == 0)
+if (f == 0)
 return (0);
-return (j);
+return (n);
 }
-
 /**
  * main - multiplies 2 numbers
  * @argc: number of arguments
@@ -48,15 +47,15 @@ return (j);
  */
 int main(int argc, char *argv[])
 {
-int r, a, b;
+int result, num1, num2;
 if (argc < 3 || argc > 3)
 {
 printf("Error\n");
 return (1);
 }
-a = _atoi(argv[1]);
-b = _atoi(argv[2]);
-r = a * b;
-printf("%d\n", r);
+num1 = _atoi(argv[1]);
+num2 = _atoi(argv[2]);
+result = num1 *num2;
+printf("%d\n", result);
 return (0);
 }
